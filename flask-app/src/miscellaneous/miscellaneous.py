11bd158372614075b394afc27358c1d05c7defc6
ethanaoki <93231153+ethanaoki@users.noreply.github.com>
2022-12-04 19:46:22 -0500

    Update miscellaneous.py

--- flask-app/src/miscellaneous/miscellaneous.py
+++ flask-app/src/miscellaneous/miscellaneous.py
@@ -20,9 +20,13 @@ def get_menu_item_names():
 def something_menu(menu_item_name):
     return execute_db('select * from ingre_menu where menu_name = \'{0}\''.format(menu_item_name))
 
-# Get everything from menu table for cooks to see on their page
+# Get everything from menu table for cooks to see on their secondary page
 @miscellaneous.route('/miscellaneous/recipes', methods=['GET'])
 def whole_menu():
     return execute_db('select * from ingre_menu')
 
+# Get all customers with active orders from table for cooks to see on main page
+@miscellaneous.route('/miscellaneous/current_orders', methods=['GET'])
+def get_current_customer_orders():
+    return execute_db('select * from customer_menu')
 
