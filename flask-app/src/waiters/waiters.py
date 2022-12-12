1c1486ce8d6795db32040cba3b3bebbd3241ec45
ethanaoki <93231153+ethanaoki@users.noreply.github.com>
2022-12-11 21:36:08 -0500

    Update waiters.py

--- flask-app/src/waiters/waiters.py
+++ flask-app/src/waiters/waiters.py
@@ -24,7 +24,7 @@ def get_customers_of_waiter():
 # Gets the meals ready to be served to customers of this waiter
 @waiters.route('/get_meals_to_serve/', methods=['GET'])
 def get_meals_to_serve():
-    return execute_db('select customer_id, id, table_num, name, menu_name' +
+    return execute_db('select cust_id, id, table_num, name, menu_name' +
                         ' from customer_menu join customers on customer_menu.cust_id = customers.cust_id ' 
                       'where customers.active = TRUE and customer_menu.fulfilled = TRUE and '
                         'customer_menu.served = FALSE and waiter_id = \''
